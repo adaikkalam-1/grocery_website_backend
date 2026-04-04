c21585d769e297fd28c13e70114ca57a6b9a8c19
Adaikkalam <adaikkalarajaa@gmail.com>
2026-04-04 14:16:46 +0530

    fix: remove partitioned attribute from cookie settings for login token

--- src/controller/auth/authController.js
+++ src/controller/auth/authController.js
@@ -41,15 +41,14 @@ const login = async (req, res) => {
       ...rest,
       token: token,
     };
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      partitioned: true,
-      path: "/",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-
+  
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",   
+   maxAge: 7 * 24 * 60 * 60 * 1000,
+});
     return res.status(200).json({ message: "Login successful", data: newData });
   } catch (error) {
     return res.status(500).json({ message: error.message });
